d385fe50bbf99eeb9dbbaaeeb310b12c40bd4f0e
Claire Liu <Claire_Liu@epam.com>
2016-05-03 16:09:38 +0800

    tutorial5

--- public/scripts/tutorial5.js
+++ public/scripts/tutorial5.js
@@ -1,11 +1,50 @@
-
+var Comment = React.createClass({
+  render: function(){
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+          {this.props.children}
+      </div>
+    );
+  }
+});
 
 var CommentList = React.createClass({
   render: function(){
     return (
-      <div className="CommentList">
-
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is one comment</Comment>
+        <Comment author="Jordan Walke">This is *another* commnent</Comment>
       </div>
-      );
+    );
   }
 });
+
+var CommentForm = React.createClass({
+  render: function(){
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+var CommentBox = React.createClass({
+  render: function(){
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
