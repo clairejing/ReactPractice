198babcd57b5b42ed5849ad602bf17bf111bdbb3
Claire Liu <Claire_Liu@epam.com>
2016-05-04 11:09:02 +0800

    tutorial14

--- public/scripts/tutorial5.js
+++ public/scripts/tutorial5.js
@@ -1,8 +1,3 @@
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 var Comment = React.createClass({
   rawMarkup: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
@@ -49,11 +44,40 @@ var CommentForm = React.createClass({
 });
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function(){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({data: data});
+      }.(this),
+      error: function(xhr, status, err){
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function(){
+    return {data:[]};
+  },
+  componentDidMount: function(){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        cosole.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   render: function(){
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
         <CommentForm />
       </div>
     );
@@ -61,6 +85,6 @@ var CommentBox = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox data={data}/>,
+  <CommentBox url="/api/comments"/>,
   document.getElementById('content')
 );
